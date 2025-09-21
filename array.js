e1873bed3188daccf706f8d35947e6459e755817
Alsaef <saef.ratul@gmail.com>
2025-09-21 21:10:06 +0600

    dev problem

--- array.js
+++ array.js
@@ -1,6 +1,36 @@
 // Using array literal notation (most common)
 
+const arr=[1,2,3,4,5]
+
+// arr.pop() // pop er modhe value dite hoi na
+
+// arr.push(6) // value dite hoi
+
+
+
+const fruits=['mango','banana','apple','orange']
+
+console.log(fruits[3]);// kon index e kon value ta ache ta dekha 
+
+const indexing=fruits.indexOf('mango') 
+
+console.log(indexing);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Using the Array constructor
 
+// const arr2= new Array(1)
 
+// console.log('arry 2',arr2);
