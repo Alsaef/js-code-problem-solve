24f4181c7ac1ad1d595b2c371d0a29782f85522a
Alsaef <saef.ratul@gmail.com>
2025-09-23 17:35:42 +0600

    dev problem

--- loop.js
+++ loop.js
@@ -3,3 +3,30 @@
 for(let num=0;num<=20;num++){
     console.log(num);
 }
+
+
+
+// for (start; end&&condition;change) {
+   
+//     // statment
+// }
+
+
+
+let num=1
+
+
+while (num<5) {
+    console.log(num);
+
+    num++
+}
+
+
+const array=[1,2,3,4,5]
+array.forEach(arr => {
+    console.log(arr);
+});
+
+
+// loop 
