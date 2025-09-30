abddbb35e1dbac874fef8f1042c46418dad190fb
Alsaef <saef.ratul@gmail.com>
2025-09-30 18:51:57 +0600

    task complete and resolve

--- functionconstacture.js
+++ functionconstacture.js
@@ -4,14 +4,42 @@
 
 // e5 e hobe window object => function => function
 
-// es6 hobe object  object => function => function
+// es6 hobe object  object => function => function / jodi methord es 5 hoi
+
+// es6 hobe window  object => function => function / jodi methord es 6 hoi
 
 // constactor function e this er value mt object
 
 // console.log(this === window); // In a browser, this will be true
 
-function fan() {
-    console.log(this);
+
+// function fan() {
+//     console.log(this);
+// }
+
+// fan()
+
+
+// const myObject={
+//     name:'al saef ratul',
+//     myFan:function () {
+//         console.log(this.name);
+//     }
+// }
+
+// console.log(myObject.name);
+
+
+const myObject={
+    name:'al saef ratul',
+    myFan:function(){
+       let fn=()=>{
+         console.log(this);
+       }
+
+       fn()
+    }
 }
 
-fan()
+console.log(myObject.myFan());
+
