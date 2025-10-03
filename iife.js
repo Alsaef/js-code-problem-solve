f4fd10f8e7a2b599e0f5052ff3da7e4d571ff2c1
Alsaef <saef.ratul@gmail.com>
2025-10-03 22:56:20 +0600

    new added

--- iife.js
+++ iife.js
@@ -7,3 +7,16 @@
 (()=>{
     console.log('es6 IIFE');  
 })();
+
+
+
+// async IIFE with await
+(async function () {
+   const data = await Promise.resolve("ES5 style async IIFE done");
+   console.log(data); 
+})();
+
+(async () => {
+   const data = await Promise.resolve("ES6 style async IIFE done");
+   console.log(data);
+})();
