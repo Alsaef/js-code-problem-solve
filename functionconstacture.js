b16bfa9c9f298dd0291013fd45591da50f8a3cc6
Alsaef <saef.ratul@gmail.com>
2025-10-10 12:43:43 +0600

    dev

--- functionconstacture.js
+++ functionconstacture.js
@@ -1,56 +1,56 @@
-// this global e this er value hoite sea window
+// // this global e this er value hoite sea window
 
-// methord object this er value object
+// // methord object this er value object
 
-// e5 e hobe window object => function => function
+// // e5 e hobe window object => function => function
 
-// es6 hobe object  object => function => function / jodi methord es 5 hoi
+// // es6 hobe object  object => function => function / jodi methord es 5 hoi
 
-// es6 hobe window  object => function => function / jodi methord es 6 hoi
+// // es6 hobe window  object => function => function / jodi methord es 6 hoi
 
-// constactor function e this er value mt object
+// // constactor function e this er value mt object
 
-// console.log(this === window); // In a browser, this will be true
+// // console.log(this === window); // In a browser, this will be true
 
 
-//  Es5 Function this Value Object
-// function fan() {
-//     console.log('Es5',this);
+// //  Es5 Function this Value Object
+// // function fan() {
+// //     console.log('Es5',this);
+// // }
+
+// // fan()
+
+// // // Es6 function this Value Window
+// // const fanTwo=()=>{
+// //   console.log("Es6",this);
+// // }
+
+// // fanTwo()
+
+
+
+// // object
+// const myObject={
+//     name:'al saef ratul',
+//     myFan:function () {
+//         // console.log(this);
+//         return this
+//     }
 // }
 
-// fan()
+// console.log(myObject.myFan());
 
-// // Es6 function this Value Window
-// const fanTwo=()=>{
-//   console.log("Es6",this);
+// // window
+
+// const myObject2={
+//     name:'al saef ratul',
+//     myFan: ()=> {
+//         // console.log(this);
+//         return this
+//     }
 // }
 
-// fanTwo()
-
-
-
-// object
-const myObject={
-    name:'al saef ratul',
-    myFan:function () {
-        // console.log(this);
-        return this
-    }
-}
-
-console.log(myObject.myFan());
-
-// window
-
-const myObject2={
-    name:'al saef ratul',
-    myFan: ()=> {
-        // console.log(this);
-        return this
-    }
-}
-
-console.log(myObject2.myFan());
+// console.log(myObject2.myFan());
 
 
 
@@ -98,21 +98,40 @@ console.log(myObject2.myFan());
 
 
 
-function myFunc(name,age) {
-   
-  this.namE=name
 
-  this.AGE=age
-
-   
-  
-
-}
-
-let myFan=new myFunc('ratul',22)
-
-console.log(myFan);
 
 // console.log(myFunc('ratul',22));
 
 // constactor functon new keyword use korle object hoiea jai
+
+// constactor function e this use korte hoi
+
+
+
+
+let myObj={
+    name:'ratul',
+
+    myfn:function(){
+        console.log('oni');
+    }
+}
+
+console.log(myObj);
+
+
+function myFanc(name) {
+
+
+    this.name=name
+
+   this.myFan= function () {
+        console.log('arafat');
+    }
+
+}
+
+
+const x=new myFanc('ratul')
+
+console.log(x);
